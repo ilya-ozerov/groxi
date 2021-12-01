4d99ee35a5bdb4059400014d41b3f827b7952945
Ilya Ozerov <efwe456lpzx>
2021-12-01 20:15:21 +0300

    Updated types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,15 +1,51 @@
+type ImageType = {
+    url: string;
+    alt: string;
+}
+
+type TagType = "Food" | "Fruits" | "Health" | "Meat" | "Dairy" | "Beverages"
+    | "Biscuits, Snacks" | "Breakfast & Dairy" | "Fruits & Vegetables" | "Home Needs"
+    | "Grocery & Staples" | "Home & Kitchen" | "Household Needs" | "Meats, Frozen"
+    | "Noodles, Sauces" | "Personal Care" | "Pet Care";
+
+type RateType = 1 | 2 | 3 | 4 | 5;
+
+type PriceType = {
+    currentPrice: number;
+    oldPrice?: number;
+}
+
+export type ReviewType = {
+    id: string;
+    name: string;
+    mark: RateType;
+    message: string;
+}
+
 export type ProductType = {
     id: string;
     name: string;
-    rate: 1 | 2 | 3 | 4 | 5;
-    image: {
-        url: string;
-        alt: string;
-    }
-    price: {
-        currentPrice: number;
-        oldPrice?: number;
-    }
+    rate: RateType;
+    image: ImageType;
+    price: PriceType;
     favorite: boolean;
     hot: boolean;
+}
+
+export type ProductDetailType = {
+    id: string;
+    name: string;
+    images: ImageType[];
+    rate: RateType;
+    price: PriceType;
+    favorite: boolean;
+    hot: boolean;
+
+    trending: boolean;
+
+    tags: TagType[];
+
+    overview: string;
+    description: string[];
+    reviews?: ReviewType[];
 }
