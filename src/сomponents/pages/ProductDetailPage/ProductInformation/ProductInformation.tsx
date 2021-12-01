6524b24f704df9dcd3bd0574712d2c0c348a2d37
Ilya Ozerov <efwe456lpzx>
2021-12-01 20:19:16 +0300

    Added <ProductInformation> component

---
+++ src/сomponents/pages/ProductDetailPage/ProductInformation/ProductInformation.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ProductDetailType, ReviewType } from '../../../../types/types';
+import './ProductInformation.scss';
+
+type ProductInformationPropsType = {
+    description: string[];
+    reviews?: ReviewType[];
+    additional?: string;
+};
+export const ProductInformation: React.FC<ProductInformationPropsType> = (props) => {
+
+
+    const information = props.description.map((str, i) => {
+        return <p key={i}>{str}</p>
+    })
+
+    return (
+        <section className="product-information">
+
+            <div className="container">
+                <div className="product-information__body">
+                    <div className="product-information__nav">
+                        <ul className="product-information__list">
+                            <li className="product-information__active">Description</li>
+                            <li>Additional Information</li>
+                            <li>Reviews</li>
+                        </ul>
+
+                    </div>
+                    <div className="product-information__info">
+                        <div className="product-information__text">
+                            {information}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    );
+}
+
