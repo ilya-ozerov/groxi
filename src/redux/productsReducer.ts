c4695bcf46795cd10ac14c0216e9e1fdf4e964e1
Ilya Ozerov <efwe456lpzx>
2021-12-07 19:57:20 +0300

    Added new actions Added thunks

--- src/redux/productsReducer.ts
+++ src/redux/productsReducer.ts
@@ -1,6 +1,7 @@
 import { AppStateType, InferActionTypes } from "./store";
 import { ThunkAction } from "redux-thunk";
-import { ProductType } from "../types/types";
+import { FilterType, ProductType } from "../types/types";
+import { productsAPI } from "../api/api";
 
 const PRODUCTS_CHANGED = 'groxi/products/PRODUCTS_CHANGED';
 const TRENDING_CHANGED = 'groxi/products/TRENDING_CHANGED';
@@ -12,6 +13,11 @@ const IS_FETCHING_FAVOURITES_SWITCHED = 'groxi/products/IS_FETCHING_FAVOURITES_S
 
 const TOTAL_PRODUCTS_COUNT_CHANGED = 'groxi/products/TOTAL_PRODUCTS_COUNT_CHANGED';
 const CURRENT_PAGE_CHANGED = 'groxi/products/CURRENT_PAGE_CHANGED';
+
+const CURRENT_FILTER_CHANGED = 'groxi/products/CURRENT_FILTER_CHANGED';
+
+const FIRST_PRODUCT_INDEX_CHANGED = 'groxi/products/FIRST_PRODUCT_INDEX_CHANGED';
+const LAST_PRODUCT_INDEX_CHANGED = 'groxi/products/LAST_PRODUCT_INDEX_CHANGED';
 
 let initialState = {
     products: [] as ProductType[],
@@ -25,6 +31,16 @@ let initialState = {
     pageSize: 9, // never changed
     totalProductsCount: 0,
     currentPage: 1,
+
+    currentFilter: {
+        price: null,
+        sorting: "default",
+        tags: [],
+    } as FilterType,
+
+
+    firstProductIndex: 0, // current index of first product on page
+    lastProductIndex: 9, // current index of last product on page
 };
 
 const productsReducer = (state = initialState, action: ActionTypes): InitialStateType => {
@@ -47,6 +63,14 @@ const productsReducer = (state = initialState, action: ActionTypes): InitialStat
             return _setTotalProductsCount(state, action.count);
         case CURRENT_PAGE_CHANGED:
             return _setCurrentPage(state, action.page);
+
+        case CURRENT_FILTER_CHANGED:
+            return _setCurrentFilter(state, action.filter);
+
+        case FIRST_PRODUCT_INDEX_CHANGED:
+            return _setFirstProductIndex(state, action.index);
+        case LAST_PRODUCT_INDEX_CHANGED:
+            return _setLastProductIndex(state, action.index);
 
         default:
             return state;
@@ -104,6 +128,26 @@ const _setCurrentPage = (state: InitialStateType, page: number): InitialStateTyp
     };
 };
 
+const _setCurrentFilter = (state: InitialStateType, filter: FilterType): InitialStateType => {
+    return {
+        ...state,
+        currentFilter: filter,
+    };
+};
+
+const _setFirstProductIndex = (state: InitialStateType, index: number): InitialStateType => {
+    return {
+        ...state,
+        firstProductIndex: index,
+    };
+};
+const _setLastProductIndex = (state: InitialStateType, index: number): InitialStateType => {
+    return {
+        ...state,
+        lastProductIndex: index,
+    };
+};
+
 export const productsActions = {
     productsChanged: (newProducts: ProductType[]) => ({ type: PRODUCTS_CHANGED, newProducts } as const),
     trendingChanged: (newProducts: ProductType[]) => ({ type: TRENDING_CHANGED, newProducts } as const),
@@ -115,7 +159,34 @@ export const productsActions = {
 
     currentPageChanged: (page: number) => ({ type: CURRENT_PAGE_CHANGED, page } as const),
     totalProductsCountChanged: (count: number) => ({ type: TOTAL_PRODUCTS_COUNT_CHANGED, count } as const),
+
+    currentFilterChanged: (filter: FilterType) => ({ type: CURRENT_FILTER_CHANGED, filter } as const),
+
+    firstProductIndexChanged: (index: number) => ({ type: FIRST_PRODUCT_INDEX_CHANGED, index } as const),
+    lastProductIndexChanged: (index: number) => ({ type: LAST_PRODUCT_INDEX_CHANGED, index } as const),
 }
+
+export const getProductsThunkCreator = (startIndex: number, endIndex: number, filter: FilterType): ThunkType =>
+    async (dispatch) => {
+        dispatch(productsActions.isFetchingProductsSwitched(true));
+
+        const payload = productsAPI.getProducts(startIndex, endIndex, filter);
+        dispatch(productsActions.productsChanged(payload.products));
+
+        dispatch(productsActions.totalProductsCountChanged(payload.totalItems));
+
+        dispatch(productsActions.isFetchingProductsSwitched(false));
+    }
+
+export const getTrendingProductsThunkCreator = (amount: number): ThunkType =>
+    async (dispatch) => {
+        dispatch(productsActions.isFetchingTrendingSwitched(true));
+
+        const products = productsAPI.getTrendingProducts(0, amount);
+        dispatch(productsActions.trendingChanged(products));
+
+        dispatch(productsActions.isFetchingTrendingSwitched(false));
+    }
 
 export type InitialStateType = typeof initialState;
 type ActionTypes = InferActionTypes<typeof productsActions>;
