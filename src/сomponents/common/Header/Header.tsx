3e5cd17415b66e27ea9b4959c68626eed812cedf
Ilya Ozerov <efwe456lpzx>
2021-12-10 17:08:33 +0300

    Fixed offset of the layout when pasting the title

--- src/сomponents/common/Header/Header.tsx
+++ src/сomponents/common/Header/Header.tsx
@@ -34,51 +34,103 @@ export const Header: React.FC<HeaderPropsType> = (props) => {
   }, []);
 
   return (
-    <header className={isStickyMod ? `${props.className} header sticky-header` : `${props.className} header`}>
-      <div className="container">
-        <div className="header__row">
-          <div className="header__logo">
-            <img src={logo} alt="groxi's logo" />
-          </div>
-          <div className="header__menu">
-            <ul className="header__list">
-              <li>
-                <NavLink to="/home">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/about">About</NavLink>
-              </li>
-              <li>
-                <NavLink to="/shop">Shop</NavLink>
-              </li>
-              <li>
-                <NavLink to="/blog">Blog</NavLink>
-              </li>
-              <li>
-                <NavLink to="/contact">Contact</NavLink>
-              </li>
-            </ul>
+    <React.Fragment>
+      <header className={`${props.className} header`}>
+        <div className="container">
+          <div className="header__row">
+            <div className="header__logo">
+              <img src={logo} alt="groxi's logo" />
+            </div>
+            <div className="header__menu">
+              <ul className="header__list">
+                <li>
+                  <NavLink to="/home">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/about">About</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/shop">Shop</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/blog">Blog</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/contact">Contact</NavLink>
+                </li>
+              </ul>
+
+            </div>
+            <div className="header__services">
+              <div className="header__icon">
+                <img src={searchIcon} alt="search icon" />
+              </div>
+              <div className="header__icon">
+                <img src={accountIcon} alt="account icon" />
+              </div>
+              <div className="header__icon">
+                <img src={wishListIcon} alt="wishlist icon" />
+              </div>
+              <div className="header__icon">
+                <img src={cartIcon} alt="cart icon" />
+              </div>
+            </div>
+
+            {!isStickyMod && <ModileMenu className="header__mobile-menu" />}
+
 
           </div>
-          <div className="header__services">
-            <div className="header__icon">
-              <img src={searchIcon} alt="search icon" />
-            </div>
-            <div className="header__icon">
-              <img src={accountIcon} alt="account icon" />
-            </div>
-            <div className="header__icon">
-              <img src={wishListIcon} alt="wishlist icon" />
-            </div>
-            <div className="header__icon">
-              <img src={cartIcon} alt="cart icon" />
-            </div>
-          </div>
-
-          <ModileMenu className="header__mobile-menu" />
-
         </div>
-      </div>
-    </header>
+      </header>
+      {isStickyMod &&
+        <header className={`${props.className} header sticky-header`}>
+          <div className="container">
+            <div className="header__row">
+              <div className="header__logo">
+                <img src={logo} alt="groxi's logo" />
+              </div>
+              <div className="header__menu">
+                <ul className="header__list">
+                  <li>
+                    <NavLink to="/home">Home</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/about">About</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/shop">Shop</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/blog">Blog</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/contact">Contact</NavLink>
+                  </li>
+                </ul>
+
+              </div>
+              <div className="header__services">
+                <div className="header__icon">
+                  <img src={searchIcon} alt="search icon" />
+                </div>
+                <div className="header__icon">
+                  <img src={accountIcon} alt="account icon" />
+                </div>
+                <div className="header__icon">
+                  <img src={wishListIcon} alt="wishlist icon" />
+                </div>
+                <div className="header__icon">
+                  <img src={cartIcon} alt="cart icon" />
+                </div>
+              </div>
+
+              <ModileMenu className="header__mobile-menu" />
+
+            </div>
+          </div>
+        </header>
+      }
+
+    </React.Fragment>
   );
 }
